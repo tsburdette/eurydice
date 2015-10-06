a60313882dbb0384fc88394d607f5cf84d51ea2d
Taylor <tburdette3@gatech.edu>
2015-10-06 10:58:23 -0400

    reverted to python 2.7

--- eurydice.py
+++ eurydice.py
@@ -13,28 +13,34 @@ channel = "#orpheus"
 nick    = "Eurydice"
 pword   = "lyric"
 
-def ping ():
-    sock.send (b"PONG : ping\n")
-
-def sendmsg (chan, msg):
-    sock.send(("PRIVMSG " + chan + " :" + msg + "\n").encode())
-
-def joinchan (chan, password):
-    sock.send(bytes("JOIN %s %s\r\n" % (chan, password), "UTF-8"))
-    
-def sendnick (nick):
-    sock.send(bytes("NICK %s\r\n" % nick,"UTF-8"))
-    
-def sendpass (password):
-    sock.send(bytes("PRIVMSG NickServ IDENTIFY %s\r\n" % password, "UTF-8"))
-
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((server, port))
 
-sock.send(bytes("NICK %s\r\n" % nick, "UTF-8"))
-sock.send(bytes("USER %s %s bla : %s\r\n" % (nick, server, nick), "UTF-8"))
-sock.send(bytes("JOIN #orphtest\r\n", "UTF-8"))
+def ping (key):
+    sock.send("PONG : " + key + "\r\n")
+
+def sendmsg (chan, msg):
+    sock.send("PRIVMSG " + chan + " :" + msg + "\r\n")
+
+def joinchan (chan, password):
+    sock.send("JOIN " + chan + " " + password + "\r\n")
+
+def senduser ():
+    sock.send("USER %s %s bla :%s\r\n" % (nick, server, nick))
+    
+def sendnick ():
+    sock.send("NICK " + nick + "\r\n")
+    
+def sendpass (password):
+    sock.send("PRIVMSG NickServ IDENTIFY " + password + "\r\n")
+
+
+
+
+senduser()
+sendnick()
+sendpass(pword)
+sock.send("JOIN #orphtest\r\n")
 sendmsg("#orphtest", "Hello! I am a bot!")
 #joinchan("#goodjobclub", "gjguys!")
 #sendmsg("#goodjobclub","Good morning, Fyurie!")
@@ -45,8 +51,7 @@ sendmsg("#orphtest", "Hello! I am a bot!")
 readbuffer = ""
 
 while true:
-    #time.sleep(5)
-    readbuffer = sock.recv(1024).decode("UTF-8")
+    readbuffer = sock.recv(1024)
     print(readbuffer)
     temp = str.split(readbuffer, "\n")
     readbuffer = temp.pop()
@@ -56,7 +61,7 @@ while true:
         line = str.split(line)
         
         if(line[0] == "PING"):
-            sock.send(bytes("PONG %s\r\n" % line[1], "UTF-8"))
+            ping(line[1])
         for index, i in enumerate(line):
             #print(*line)
             if(".quit" in line[index]):
